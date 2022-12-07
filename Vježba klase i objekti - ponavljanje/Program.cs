46c28ca857eddd7965c4a3420460ffe618ea7032
ClashMania <113007885+ClashMania@users.noreply.github.com>
2022-12-07 08:15:55 +0100

    Update Program.cs

--- Vježba klase i objekti - ponavljanje/Program.cs
+++ Vježba klase i objekti - ponavljanje/Program.cs
@@ -1,50 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Vježba_klase_i_objekti___ponavljanje
+namespace KlaseIObjekti3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Stablo stablo = new Stablo();
-            
+            Stablo stablo1 = new Stablo(true);
+            Stablo stablo2 = new Stablo(false);
 
-          
+            Console.WriteLine("Stablo 1: " + stablo1.ToString());
+            Console.WriteLine("Stablo 2: " + stablo2.ToString());
+
+            Console.ReadKey();
         }
-
-       
+        
     }
     class Stablo
     {
-        bool otpadajuListovi;
+        bool odpadajuListovi;
         string vrstaStabla;
+
+        public bool OdpadajuListovi { get => odpadajuListovi; set => odpadajuListovi = value; }
 
         public override string ToString()
         {
-            return "Stablo je " + this.vrstaStabla;
+            return vrstaStabla;
         }
-        public Stablo(bool otpadajuListovi0)
+
+        public Stablo()
         {
-               this.otpadajuListovi = otpadajuListovi;
-               if(otpadajuListovi)
-                {
-                this.vrstaStabla = "Bijelogorično";
-                }
-                else
-                {
-                this.vrstaStabla = "Crnogorično";
-            
-                }
+
         }
-        
-        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi=value;}
-        public string vrstaStabla { get => vrstaStabla; set => vrstaStabla = value; }
+        public Stablo(bool odpadajuListovi)
+        {
+            this.odpadajuListovi = odpadajuListovi;
 
-
+            if (odpadajuListovi)
+            {
+                this.vrstaStabla = "Vrsta stabla je bijelogorično";
+            }
+            else
+            {
+                this.vrstaStabla = "Vrsta stabla je crnogorično";
+            }
+        }
 
     }
 }
